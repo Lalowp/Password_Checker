1e8007484e488b50b115fedb2da7e6112333e27b
Eduardo Labastida Arce <67454928+Lalowp@users.noreply.github.com>
2020-08-26 23:58:33 -0500

    Add files via upload

---
+++ passwordChecker.py
@@ -0,0 +1,44 @@
+import requests
+import hashlib
+
+try:
+    with open('password.txt', mode='r') as password:
+        text = password.readlines()
+except FileNotFoundError as error:
+    print('Check the file path')
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    #check password if it exist in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        password_len  = len(password)
+        passwrod_star = password_len* '*' 
+        if count:
+            print(f'{passwrod_star} was found {count} times, you should change it')
+        else:
+            print(f'{passwrod_star} was not found. Use it!')
+
+if __name__ == '__main__':
+    main(text)
+
